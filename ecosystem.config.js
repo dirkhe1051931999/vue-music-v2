9ef64ec1921419b7e140e241574f18ecee788416
dirkhe1051931999 <hejiandirk@163.com>
2025-03-20 20:54:33 +0800

    feat(config): Enhance PM2 configuration for production environment and logging

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,16 +3,20 @@ module.exports = {
     {
       name: '163music-proxy',
       script: 'bundle.js',
-      instances: 1,
       env: {
         NODE_ENV: 'development',
       },
       env_production: {
         NODE_ENV: 'production',
       },
+      exec_mode: 'cluster',
+      instances: 1,
       autorestart: true,
       max_restarts: 10,
       min_uptime: '1m',
+      out_file: './logs/out.log', // 指定标准输出日志文件
+      error_file: './logs/error.log', // 指定错误输出日志文件
+      log_date_format: 'YYYY-MM-DD HH:mm Z', // 日志时间格式
     },
   ],
 };
