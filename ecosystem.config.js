d8b4bdbae3773f6fb6204ceffc9505c5b812eea2
dirkhe1051931999 <hejiandirk@163.com>
2025-03-20 20:48:32 +0800

    chore(deploy): Update deployment configuration for new server credentials

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,18 @@
 module.exports = {
-  apps: [{
-    name: "163music-proxy",
-    script: "bundle.js",
-    instances: 1,
-    env: {
-      NODE_ENV: "development",
+  apps: [
+    {
+      name: '163music-proxy',
+      script: 'bundle.js',
+      instances: 1,
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+      autorestart: true,
+      max_restarts: 10,
+      min_uptime: '1m',
     },
-    env_production: {
-      NODE_ENV: "production",
-    },
-    autorestart: true,
-    max_restarts: 10,
-    min_uptime: "1m"
-  }]
-}
+  ],
+};
